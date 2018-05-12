f87d5e5b92467a99fd27b1ea3723bd27f0756348
Han Ban <hannah15198@gmail.com>
2018-05-11 23:49:24 -0700

    added moment()

--- javascript/app.js
+++ javascript/app.js
@@ -1,21 +1,24 @@
 $(document).ready(function () {
 
-   
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyA5leULUE1AfLkB6d5oDKqhuWtFV64Nu_g",
+      authDomain: "trainsch-a9808.firebaseapp.com",
+      databaseURL: "https://trainsch-a9808.firebaseio.com",
+      projectId: "trainsch-a9808",
+      storageBucket: "trainsch-a9808.appspot.com",
+      messagingSenderId: "537354270642"
+    };
+    firebase.initializeApp(config);
+ 
     var database = firebase.database();
 
     // initial values
-    trainName = $("#train-name-input").val().trim();
-    destination = $("#destination-input").val().trim();
-    firstTrainTime = $("#first-train-input").val().trim();
-    frequency = $("#frequency-input").val().trim();
+    var trainName = $("#train-name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var firstTrainTime = $("#first-train-input").val().trim();
+    var frequency = $("#frequency-input").val().trim();
     var nextOne = firstTrainTime + frequency;
-
-    // var diffTime = moment().diff(moment.unix(firstTrainTime), "minutes");
-    // var timeRemainder = moment().diff(moment.unix(firstTrainTime), "minutes") % frequency;
-    // var minutes = frequency - timeRemainder;
-
-    // var nextArrival = moment().add(minutes, "m".format("hh:mm");
-    // var minutesAway = moment();
     
     var result = moment(nextOne).diff(moment(firstTrainTime));
     var humanize = moment.duration(result).humanize();
@@ -46,6 +49,33 @@ $(document).ready(function () {
         console.log(childSnapshot.val().nextArrival);
         console.log(childSnapshot.val().minutesAway);
 
+        // Store everything into a variable.
+  trainName = childSnapshot.val().name;
+  destination = childSnapshot.val().destination;
+  frequency = childSnapshot.val().frequency;
+  firstTrainTime = childSnapshot.val().firstTrain;
+
+  timeArray = firstTrainTime.split(":");
+  trainTime = moment().hours(timeArray[0]).minutes(timeArray[1]);
+  maxMoment = moment.max(moment(), trainTime);
+  tMinutes;
+  tArrival;
+
+ 
+ if (maxMoment === trainTime) {
+   tArrival = trainTime.format("hh:mm A");
+   tMinutes = trainTime.diff(moment(), "minutes");
+ } else {
+
+   var differenceTimes = moment().diff(trainTime, "minutes");
+   var tRemainder = differenceTimes % tFrequency;
+   tMinutes = tFrequency - tRemainder;
+
+
+   tArrival = moment().add(tMinutes, "m").format("hh:mm A");
+ }
+ console.log("tMinutes:", tMinutes);
+ console.log("tArrival:", tArrival);
         // log everything that is coming out of the snapshot
         $("#train-table").append("<tr><th>" + trainName + "</th><th>" + destination + "</th><th>" + firstTrainTime + "</th><th>" + frequency + "</th><th>" + nextArrival + "</th><th>" + minutesAway + 
     "</th></tr>");
@@ -55,3 +85,7 @@ $(document).ready(function () {
     // change html
 
 })
+
+// first train plus the current time in minutes
+// remainder of the time difference
+// frequency minus remainder
